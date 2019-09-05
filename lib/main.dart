f813890ce3a98eda184c6a661f1bb61ec7076074
Vicky Leu <yu6564172@gmail.com>
2019-09-05 18:43:30 +0800

    pingfang fonts

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,9 @@ import 'newPage/Home/HomePage.dart';
 import 'page/demo/page.dart';
 
 void main() {
+  FlutterError.onError = (FlutterErrorDetails details, { bool forceReport: false }) {
+    FlutterError.dumpErrorToConsole(details, forceReport: true);
+  };
   runApp(MyApp());
   if (Platform.isAndroid) {
     // 以下两行 设置android状态栏为透明的沉浸。写在组件渲染之后，是为了在渲染后进行set赋值，覆盖状态栏，写在渲染之前MaterialApp组件会覆盖掉这个值。
--- lib/newPage/Top/TopPage.dart
+++ lib/newPage/Top/TopPage.dart
@@ -1,4 +1,5 @@
 import 'dart:collection';
+import 'dart:ui' as prefix0;
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -34,6 +35,7 @@ class TopState extends State<TopPage> with SingleTickerProviderStateMixin {
               pinned: true,
               backgroundColor: const Color(0XFFF76F6F),
               flexibleSpace: FlexibleSpaceBar(
+                collapseMode: CollapseMode.pin,
                 background: Container(
                   width: double.infinity,
                   height: double.infinity,
@@ -43,12 +45,11 @@ class TopState extends State<TopPage> with SingleTickerProviderStateMixin {
                       Padding(
                         padding: EdgeInsets.only(bottom: 20),
                         child:
-//                        Image.asset("assets/images/wxbg.png")
                             SvgPicture.asset("assets/images/progress_bar8.svg"),
                       ),
                       Padding(
                         child: Column(
-                          mainAxisSize:MainAxisSize.min ,
+                          mainAxisSize: MainAxisSize.min,
                           mainAxisAlignment: MainAxisAlignment.center,
                           children: <Widget>[
                             Text(
@@ -66,10 +67,9 @@ class TopState extends State<TopPage> with SingleTickerProviderStateMixin {
                             Text(
                               '总题数：6498631',
                               style:
-                              TextStyle(fontSize: 10, color: Colors.white),
+                                  TextStyle(fontSize: 10, color: Colors.white),
                               textAlign: TextAlign.center,
                             ),
-
                           ],
                         ),
                         padding: EdgeInsets.only(bottom: 35),
@@ -77,48 +77,126 @@ class TopState extends State<TopPage> with SingleTickerProviderStateMixin {
                     ],
                   ),
                 ),
-//                Image.asset("assets/images/wxbg.png"),
               ),
               expandedHeight:
                   MediaQuery.of(context).size.width.toDouble() * 0.6,
             ),
-            SliverList(
-              delegate: SliverChildBuilderDelegate(
-                (BuildContext context, int index) {
-                  if (map[index] == null)
-                    map[index] = new GlobalKey<AppExpansionTileState>();
-                  final key = map[index];
-                  return Container(
-                    alignment: Alignment.center,
+            SliverPersistentHeader(
+              // 可以吸顶的TabBar
+              pinned: true,
+              delegate: _StickyContainerDelegate(
+                  child: PreferredSize(
+                      preferredSize: Size.fromHeight(52.0),
+                      child: Stack(
+                        children: <Widget>[
+                          Container(
+                            color: const Color(0XFFF76F6F),
+                            height: 32,
+                          ),
+                          Padding(
+                            padding: EdgeInsets.only(top: 32),
+                            child: Container(
+                              color: Colors.transparent,
+                              height: 20,
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.only(left: 15, right: 15),
+                            child: Container(
+                              height: double.infinity,
+                              decoration: BoxDecoration(
+                                color: Colors.white,
+                                borderRadius: BorderRadius.circular(8),
+                                boxShadow: [
+                                  BoxShadow(
+                                    offset: Offset(0, 3),
+                                    color: Color(0XFFE6E6E6),
+                                    spreadRadius: 0.1,
+                                    blurRadius: 8,
+                                  )
+                                ],
+                              ),
+                              child: Center(
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: <Widget>[
+                                    Expanded(
+                                      child: Center(
+                                        child: Text(
+                                          '章节练习',
+                                          style: TextStyle(
+                                              fontSize: 15,
+                                              fontFamily:'PingFang-SC-Regular.ttf',
+                                              color: Color(0XFFf76f6f)),
+                                        ),
+                                      ),
+                                    ),
+                                    Padding(
+                                      padding:
+                                          EdgeInsets.only(top: 14, bottom: 14),
+                                      child: VerticalDivider(),
+                                    ),
+                                    Expanded(
+                                      child: Center(
+                                        child: Text(
+                                          '题型练习',
+                                          style: TextStyle(
+                                              fontSize: 15,
+                                              fontFamily:'PingFang',
+                                              fontWeight: FontWeight.normal,
+                                              color: Color(0Xff333333)),
+                                        ),
+                                      ),
+                                    )
+                                  ],
+                                ),
+                              ),
+                            ),
+                          )
+                        ],
+                      ))),
+            ),
+
+
+              SliverList(
+                delegate: SliverChildBuilderDelegate(
+                  (BuildContext context, int index) {
+                    if (map[index] == null)
+                      map[index] = new GlobalKey<AppExpansionTileState>();
+                    final key = map[index];
+                    return Container(
+                      alignment: Alignment.center,
 //                  color: Colors.lightBlue[100 * (index % 9)],
-                    child: AppExpansionTile(
-                      key: key,
-                      title: Text('你们啊,naive $index'),
-                      initiallyExpanded: selectExpandIndex == index,
-                      //默认是否展开
-                      onExpansionChanged: (expand) {
-                        if (expand) {
-                          setState(() {
-                            selectExpandIndex = index;
-                            map.forEach((i, state) {
-                              if (i != index) {
-                                state?.currentState?.collapse();
-                              }
+                      child: AppExpansionTile(
+                        key: key,
+                        title: Text('你们啊,naive $index'),
+                        initiallyExpanded: selectExpandIndex == index,
+                        //默认是否展开
+                        onExpansionChanged: (expand) {
+                          if (expand) {
+                            setState(() {
+                              selectExpandIndex = index;
+                              map.forEach((i, state) {
+                                if (i != index) {
+                                  state?.currentState?.collapse();
+                                }
+                              });
                             });
-                          });
-                        }
-                      },
-                      children: ["试试看啊", "试看", "看"]
-                          .map((val) => new ListTile(
-                                title: new Text(val),
-                              ))
-                          .toList(),
-                    ),
-                  );
-                },
-                childCount: 20,
+                          }
+                        },
+                        children: ["试试看啊", "试看", "看"]
+                            .map((val) => new ListTile(
+                                  title: new Text(val),
+                                ))
+                            .toList(),
+                      ),
+                    );
+                  },
+                  childCount: 200,
+                ),
               ),
-            )
+
+
           ],
         ),
         PreferredSize(
@@ -142,4 +220,27 @@ class TopState extends State<TopPage> with SingleTickerProviderStateMixin {
       ],
     ));
   }
+}
+
+class _StickyContainerDelegate extends SliverPersistentHeaderDelegate {
+  final PreferredSize child;
+
+  _StickyContainerDelegate({@required this.child});
+
+  @override
+  Widget build(
+      BuildContext context, double shrinkOffset, bool overlapsContent) {
+    return this.child;
+  }
+
+  @override
+  bool shouldRebuild(SliverPersistentHeaderDelegate oldDelegate) {
+    return false;
+  }
+
+  @override
+  double get maxExtent => child.preferredSize.height;
+
+  @override
+  double get minExtent => child.preferredSize.height;
 }
